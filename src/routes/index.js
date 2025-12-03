e0d264eac83af187fe7906b3fe2a228502d8d7de
Thomas Kudin <chaffeescout@gmail.com>
2025-12-03 14:52:38 +0300

    fix vue router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router'
 import Home from '@/pages/home/Home.vue';
-import Contacts from "@/pages/contacts/Contacts.vue";
 import Vacancies from "@/pages/vacancies/Vacancies.vue";
 
 export const routes = [
@@ -13,8 +12,11 @@ export const routes = [
         component: Vacancies,
     },
 ];
+const isServer = typeof window === 'undefined'
+
+const history = isServer ? createMemoryHistory(import.meta.env.BASE_URL) : createWebHistory(import.meta.env.BASE_URL)
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: history,
     routes,
 });
