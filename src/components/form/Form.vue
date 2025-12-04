4de6b0ab2d963036c64f0247469b22ff4954df0a
Thomas Kudin <chaffeescout@gmail.com>
2025-12-04 15:08:07 +0300

    add style for vacancies

---
+++ src/components/form/Form.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+    name: 'Form'
+}
+</script>
+
+<template>
+    <div id="form" class="vForm">
+        <div class="vForm__wrapper">
+            <h2 class="vForm__title">Заполни анкету</h2>
+            <form class="vForm__body">
+                <div class="vForm__group">
+                    <label for="position" class="vForm__label">Желаемая должность</label>
+                    <select id="position" name="position" required class="vForm__select">
+                        <option value="intern_restaurant_worker">Работник ресторана-стажёр</option>
+                        <option value="restaurant_worker">Работник ресторана</option>
+                        <option value="coach_instructor">Тренер-инструктор</option>
+                        <option value="restaurant_manager">Менеджер ресторана</option>
+                    </select>
+                </div>
+                <div class="vForm__group">
+                    <label for="name" class="vForm__label">Имя</label>
+                    <input placeholder="Имя" type="text" id="name" name="name" required class="vForm__input">
+                </div>
+                <div class="vForm__group">
+                    <label for="age" class="vForm__label">Возраст</label>
+                    <input placeholder="Возраст" type="number" id="age" name="age" required class="vForm__input">
+                </div>
+                <div class="vForm__group">
+                    <label for="phone" class="vForm__label">Телефон</label>
+                    <input placeholder="Телефон" type="tel" id="phone" name="phone" required class="vForm__input">
+                </div>
+                <div class="vForm__group vForm__group--checkbox">
+                    <input type="checkbox" id="agreement" name="agreement" required class="vForm__checkbox">
+                    <label for="agreement" class="vForm__label--agreement">Даю своё согласие на обработку персональных данных</label>
+                </div>
+                <button type="button" class="vForm__submit-button">Отправить</button>
+            </form>
+        </div>
+
+    </div>
+</template>
+
+<style src="./form.css" scoped>
+
+</style>
---
+++ src/components/jobs/item/JobsItem.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    name: "JobsItem",
+
+    props: ['title', 'text'],
+
+    data() {
+        return {
+            isOpened: false,
+        }
+    },
+
+    methods: {
+        onToggleJobsItem() {
+            this.isOpened = !this.isOpened;
+        }
+    },
+
+    directives: {
+        spoiler: {
+            mounted(el) {
+                el.style.height = el.scrollHeight + 'px';
+            },
+            beforeUnmount(el) {
+                el.style.height = el.scrollHeight + 'px';
+            }
+        },
+    }
+}
+</script>
+
+<template>
+    <div class="vJobsItem">
+        <div class="vJobsItem__wrapper">
+            {{title}}
+            <div class="vJobsItem__button"
+                 :class="{'vJobsItem__button--minus': isOpened}"
+                 @click="onToggleJobsItem"></div>
+        </div>
+        <transition name="spoiler">
+            <div v-if="isOpened" class="vJobsItem__text" v-spoiler>
+                <div class="vJobsItem__textHtml" v-html="text"></div>
+                <a href="#form" class="vJobsItem__btn">Отклинуться</a>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<style src="./jobs-item.css">
+</style>
